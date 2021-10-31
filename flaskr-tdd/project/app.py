f4a274d055cd3c9e844c29273333e7de5d72a041
Yu Xin Cheng <yx.cheng73@gmail.com>
2021-10-31 18:07:18 -0400

    postgres heroku

--- flaskr-tdd/project/app.py
+++ flaskr-tdd/project/app.py
@@ -1,7 +1,7 @@
 import sqlite3
 from pathlib import Path
 from functools import wraps
-
+import os
 from flask import Flask, g, render_template, request, session, \
                   flash, redirect, url_for, abort, jsonify
 from flask_sqlalchemy import SQLAlchemy
@@ -14,7 +14,11 @@ DATABASE = "flaskr.db"
 USERNAME = "admin"
 PASSWORD = "admin"
 SECRET_KEY = "change_me"
-SQLALCHEMY_DATABASE_URI = f'sqlite:///{Path(basedir).joinpath(DATABASE)}'
+SQLALCHEMY_DATABASE_URI = os.getenv(
+    'DATABASE_URL',
+    f'sqlite:///{Path(basedir).joinpath(DATABASE)}'
+)
+
 SQLALCHEMY_TRACK_MODIFICATIONS = False
 
 
